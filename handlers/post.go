d23448ce08011623a7c2d20997e07f7b3a3e9da2
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-08-25 15:55:37 +0700

    Return representation of created resource and Location header

--- handlers/post.go
+++ handlers/post.go
@@ -2,12 +2,15 @@ package handlers
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/peyuaa/segmentify/data"
 )
 
 func (s *Segments) Create(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "application/json")
 	// fetch the segment from the context
 	segment := r.Context().Value(KeySegment{}).(data.Segment)
 
@@ -17,15 +20,57 @@ func (s *Segments) Create(rw http.ResponseWriter, r *http.Request) {
 
 	switch {
 	case err == nil:
-		rw.WriteHeader(http.StatusCreated)
 	case errors.Is(err, data.ErrSegmentAlreadyExists):
 		rw.WriteHeader(http.StatusConflict)
 		err := data.ToJSON(&GenericError{Message: err.Error()}, rw)
 		if err != nil {
 			s.l.Error("Unable to serialize GenericError", "error", err)
 		}
+		return
 	default:
 		s.l.Error("Unable to insert segment", "error", err)
 		rw.WriteHeader(http.StatusInternalServerError)
+		err := data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		if err != nil {
+			s.l.Error("Unable to serialize GenericError", "error", err)
+		}
+		return
+	}
+
+	// retrieve segment to include the result of the operation in the response body
+	segment, err = s.d.GetSegmentBySlug(r.Context(), segment.Slug)
+
+	switch {
+	case err == nil:
+	case errors.Is(err, data.ErrSegmentNotFound):
+		s.l.Error("Unable to find created segment", "error", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		err := data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		if err != nil {
+			s.l.Error("Unable to serialize GenericError", "error", err)
+		}
+		return
+	default:
+		s.l.Error("Unable to find segment", "error", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		err := data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		if err != nil {
+			s.l.Error("Unable to serialize GenericError", "error", err)
+		}
+		return
+	}
+
+	// set the Location header to the URL of the newly created resource
+	u := &url.URL{
+		Scheme: "http",
+		Host:   r.Host,
+		Path:   fmt.Sprintf("/segments/%s", segment.Slug),
+	}
+	rw.Header().Add("Location", u.String())
+
+	rw.WriteHeader(http.StatusCreated)
+	err = data.ToJSON(segment, rw)
+	if err != nil {
+		s.l.Error("Unable to serialize segment", "error", err)
 	}
 }
